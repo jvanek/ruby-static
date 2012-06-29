ec5d2f8304176cc7eef7665550a818cb4c7cfee9
josef.vanek <josef.vanek@intellicore.net>
2012-06-29 10:16:07 +0200

    Initial Commit

---
+++ ruby-static/ruby_static.h
@@ -0,0 +1,13 @@
+//
+//  ruby_static.h
+//  ruby-static
+//
+//  Created by Vaněk Josef on 29/06/12.
+//  Copyright (c) 2012 Intellicore. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ruby_static : NSObject
+
+@end
---
+++ ruby-static/ruby_static.m
@@ -0,0 +1,13 @@
+//
+//  ruby_static.m
+//  ruby-static
+//
+//  Created by Vaněk Josef on 29/06/12.
+//  Copyright (c) 2012 Intellicore. All rights reserved.
+//
+
+#import "ruby_static.h"
+
+@implementation ruby_static
+
+@end
