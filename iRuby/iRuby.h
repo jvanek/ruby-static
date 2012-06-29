116449c932210b580677f8ab182b75800d165ac0
josef.vanek <josef.vanek@intellicore.net>
2012-06-29 10:49:11 +0200

    Added new framework target

---
+++ iRuby/iRuby.h
@@ -0,0 +1,13 @@
+//
+//  iRuby.h
+//  iRuby
+//
+//  Created by Vaněk Josef on 29/06/12.
+//  Copyright (c) 2012 Intellicore. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface iRuby : NSObject
+
+@end
---
+++ iRuby/iRuby.m
@@ -0,0 +1,13 @@
+//
+//  iRuby.m
+//  iRuby
+//
+//  Created by Vaněk Josef on 29/06/12.
+//  Copyright (c) 2012 Intellicore. All rights reserved.
+//
+
+#import "iRuby.h"
+
+@implementation iRuby
+
+@end
